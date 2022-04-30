ca2044930978d1cdcf4240ee5135bccf419ed3d5
William Berg <williamberg567@gmail.com>
2022-04-30 12:17:22 -0300

    implementando testes unitários e ajustes ao projeto

--- src/Templates/Home/Home.jsx
+++ src/Templates/Home/Home.jsx
@@ -38,6 +38,8 @@ export default function App(){
     };
 
     const handleChangeOption = async (e) => {
+        setAllReturnedData([]);
+
         const { selectedIndex } = e.target;
         const selectedOption = renderOptions[selectedIndex];
 
